cdbf7e76fa3a9654bdcf1f2370727aac27a7748c
arslanah99 <Arah09@hotmail.com>
2019-02-12 23:04:22 +0000

    added custom urls

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,14 +19,21 @@ app.get('/urls', (req, res) => {
     res.render('urls_index', templateVars);
 });
 
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
 app.get('/urls/:shortURL', (req, res) => {
     let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase};
     res.render('urls_show', templateVars)
 })
 
-app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
+app.post('/urls', (req, res) => {
+    console.log(req.body);
+    res.send('Ok')
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
